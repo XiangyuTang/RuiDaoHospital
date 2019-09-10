f2ce92ad3d2645a113b9d6e2921aae996a3a2c2b
Jmiaomiao <49583570+Jmiaomiao@users.noreply.github.com>
2019-09-10 23:04:41 +0800

    Update registerForm.vue

--- src/views/register/component/registerForm.vue
+++ src/views/register/component/registerForm.vue
@@ -5,21 +5,25 @@
     </div>
     <div>
       <span style="font-style: italic"><strong>患者信息</strong></span>
-      <b-button class="pull-right" type="submit" size="sm" variant="primary" @click="submit"><i class="fa fa-dot-circle-o"></i> Submit</b-button>
-      <b-button class="pull-right" type="reset" size="sm" variant="danger" @click="reset"><i class="fa fa-ban"></i> Reset</b-button>
+      <b-button class="pull-right" type="submit" size="sm" variant="primary" @click="submit"><i class="fa fa-dot-circle-o"></i> 提交</b-button>
+      <b-button class="pull-right" type="reset" size="sm" variant="danger" @click="reset"><i class="fa fa-ban"></i> 清空</b-button>
     </div>
     <br/>
     <div style="background:linear-gradient(to left,#efefef,#b6b6b6);height:1px;"></div>
     <br/>
 
     <b-form>
+
       <b-row>
-        <b-col md="6">
+      <b-col md="1">
+        <b-button variant="outline-Dark" class="btn btn-outline-dark btn-block" @click="getPatient">身份证读卡</b-button>
+      </b-col>
+        <b-col md="3">
           <b-form-group
             description=""
-            label="患者身份证号"
+            label="身份证号"
             label-for="patientIdentity"
-            :label-cols="3"
+            :label-cols="2"
             :horizontal="true">
             <b-form-input :state="idState" v-model="patient.patientIdentity" id="patientIdentity" type="text" placeholder="请输入患者身份证号"></b-form-input>
             <b-form-invalid-feedback id="patientIdentity">
@@ -27,52 +31,20 @@
             </b-form-invalid-feedback>
           </b-form-group>
         </b-col>
-        <b-col md="1">
-          <b-button variant="outline-Dark" class="btn btn-outline-dark btn-block" @click="getPatient">查询</b-button>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col md="6">
+        <b-col md="3">
           <b-form-group
             description=""
             label="患者姓名"
             label-for="patientName"
-            :label-cols="3"
+            :label-cols="2"
             :horizontal="true">
             <b-form-input :state="nameState" v-model="patient.patientName" id="patientName" type="text" autocomplete="name" placeholder="请输入患者姓名"></b-form-input>
             <b-form-invalid-feedback id="patientIdentity">
               请输入两字以上姓名
             </b-form-invalid-feedback>
           </b-form-group>
-          <b-form-group
-            label="患者性别"
-            label-for="patientGender"
-            :label-cols="3"
-            :horizontal="true">
-            <b-form-radio-group
-              id="patientGender"
-              name="patientGender">
-              <div class="custom-control custom-radio custom-control-inline">
-                <input v-model="patient.patientGender" type="radio" id="male" name="patientGender" class="custom-control-input" value="1" checked>
-                <label class="custom-control-label" for="male">男</label>
-              </div>
-              <div class="custom-control custom-radio custom-control-inline">
-                <input v-model="patient.patientGender" type="radio" id="female" name="patientGender" class="custom-control-input" value="2">
-                <label class="custom-control-label" for="female">女</label>
-              </div>
-            </b-form-radio-group>
-          </b-form-group>
         </b-col>
-      </b-row>
-      <b-row>
-        <b-col md="6">
-          <b-form-group
-            label="患者生日" label-for="patientBirth"
-            :label-cols="3"
-            :horizontal="true">
-            <b-form-input v-model="patient.patientBirth" type="date" id="patientBirth"></b-form-input>
-          </b-form-group>
-        </b-col>
+
       </b-row>
 
       <div>
@@ -83,11 +55,11 @@
       <br/>
 
       <b-row>
-        <b-col md="6">
+        <b-col md="3">
           <b-form-group
             label="选择科室"
             label-for="selectDepartment"
-            :label-cols="3"
+            :label-cols="2"
             :horizontal="true">
             <b-form-select id="selectDepartment"
                            size="lg"
@@ -96,11 +68,13 @@
               <option v-for="department in departmentList" :key="department.departmentId" :value="department.departmentId" :label="department.departmentName"></option>
             </b-form-select>
           </b-form-group>
+        </b-col>
+          <b-col md="3">
           <b-form-group
             description="选填"
             label="选择医生"
             label-for="selectDoctor"
-            :label-cols="3"
+            :label-cols="2"
             :horizontal="true">
             <b-form-select id="selectDoctor"
                            size="lg"
@@ -111,51 +85,38 @@
           </b-form-group>
         </b-col>
       </b-row>
-
       <b-row>
-        <b-col md="6">
+        <b-col md="3">
           <b-form-group
             label="结算类别"
             label-for="calculationType"
-            :label-cols="3"
+            :label-cols="2"
             :horizontal="true">
-            <b-form-radio-group
-              id="calculationType"
-              name="calculationType">
-              <div class="custom-control custom-radio custom-control-inline">
-                <input v-model="registration.calculationTypeId" type="radio" id="ownExpense" name="calculationType" class="custom-control-input" value="1" checked>
-                <label class="custom-control-label" for="ownExpense">自费</label>
-              </div>
-              <div class="custom-control custom-radio custom-control-inline">
-                <input v-model="registration.calculationTypeId" type="radio" id="medicalInsurance" name="calculationType" class="custom-control-input" value="2">
-                <label class="custom-control-label" for="medicalInsurance">医保</label>
-              </div>
-              <div class="custom-control custom-radio custom-control-inline">
-                <input v-model="registration.calculationTypeId" type="radio" id="nrcms" name="calculationType" class="custom-control-input" value="3">
-                <label class="custom-control-label" for="nrcms">新农合</label>
-              </div>
-            </b-form-radio-group>
+            <b-form-select id="selectCalculationType"
+                           size="lg"
+                           :plain="true">
+              <option>自费</option>
+              <option>医保</option>
+              <option>社保</option>
+            </b-form-select>
           </b-form-group>
+        </b-col>
+          <b-col md="3">
           <b-form-group
             label="挂号级别"
             label-for="registrationLevel"
-            :label-cols="3"
+            :label-cols="2"
             :horizontal="true">
             <b-form-radio-group
               id="registrationLevel"
               name="registrationLevel">
-              <div class="custom-control custom-radio custom-control-inline">
-                <input v-model="registration.registrationLevelId" type="radio" id="general" name="registrationLevel" class="custom-control-input" value="1" checked>
-                <label class="custom-control-label" for="general">普通</label>
-              </div>
-              <div class="custom-control custom-radio custom-control-inline">
-                <input v-model="registration.registrationLevelId" type="radio" id="expert" name="registrationLevel" class="custom-control-input" value="2">
-                <label class="custom-control-label" for="expert">专家</label>
-              </div>
-              <div class="custom-control custom-radio custom-control-inline">
-                <input v-model="registration.registrationLevelId" type="radio" id="emergency" name="registrationLevel" class="custom-control-input" value="3">
-                <label class="custom-control-label" for="emergency">急诊</label>
-              </div>
+              <b-form-select id="selectRegistrationLevel"
+                             size="lg"
+                             :plain="true">
+                <option>普通</option>
+                <option>专家</option>
+                <option>特需</option>
+               </b-form-select>
             </b-form-radio-group>
           </b-form-group>
         </b-col>
@@ -166,18 +127,18 @@
           <b-form-group
             label="是否购买病历本?"
             label-for="buyMedical"
-            :label-cols="3"
+            :label-cols="2"
             :horizontal="true">
             <b-form-radio-group
               id="buyMedical"
               name="customRadioInline1">
               <div class="custom-control custom-radio custom-control-inline">
                 <input v-model="registration.buyMedicalRecord" type="radio" id="yes" name="customRadioInline1" class="custom-control-input" value="1" checked>
-                <label class="custom-control-label" for="yes">否</label>
+                <label class="custom-control-label" for="yes">是</label>
               </div>
               <div class="custom-control custom-radio custom-control-inline">
                 <input v-model="registration.buyMedicalRecord" type="radio" id="no" name="customRadioInline1" class="custom-control-input" value="2">
-                <label class="custom-control-label" for="no">是</label>
+                <label class="custom-control-label" for="no">否</label>
               </div>
             </b-form-radio-group>
           </b-form-group>
