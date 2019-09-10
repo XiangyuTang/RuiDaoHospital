d89ea23a287abf4000c88aaa90407da13b78aea5
Jmiaomiao <49583570+Jmiaomiao@users.noreply.github.com>
2019-09-10 21:49:49 +0800

    Update register.js

--- src/nav/register.js
+++ src/nav/register.js
@@ -1,14 +1,18 @@
 export default [
   {
-    name: '挂号首页',
+    name: '挂号',
     url: '/register/registerHome'
   },
   {
-    name: '收费首页',
+    name: '收费',
     url: '/register/chargeHome'
   },
   {
-    name: '患者费用查询',
-    url: '/register/chargeHome'
+    name: '开具发票',
+    url: '/register/billHome'
+  },
+  {
+    name: '收费员日结',
+    url: '/register/checkHome'
   },
 ]
